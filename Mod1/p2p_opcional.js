cafc20a9ed8e9af3234e396164f55209fa32c7b5
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-05-18 16:58:32 +0200

    Ejercicios Módulos 1 y 2

---
+++ Mod1/p2p_opcional.js
@@ -0,0 +1,3 @@
+var pi = Math.PI.toFixed(8); // Primeros ocho decimales de pi
+var ristra = "\u55e8\uff0c\u4f60\u597d\u5417"; // Ristra de caracteres chinos
+console.log(pi + "\n\n" + ristra); // Imprimir resultado
